abaa19dd2ae83519f9a7cdc2cf0ff39544449f4b
Vinit Rai <learner.vinit@gmail.com>
2018-12-29 13:53:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
 const main = document.getElementById('main');
- 
+
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 const input = document.querySelector('input');
- 
+
 input.addEventListener('keydown', function(e) {
   console.log(e.key);
+});
+
+const input2 = document.querySelector('input')
+ 
+input2.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
 });
